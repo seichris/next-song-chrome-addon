af9fdcd963ca951bbe525d3a3a8c31f02399b196
Chris <25006584+seichris@users.noreply.github.com>
2023-02-06 18:48:35 +0100

    Update background.js

--- src/bg/background.js
+++ src/bg/background.js
@@ -13,15 +13,7 @@ chrome.commands.onCommand.addListener(function(command) {
         navigator.mediaSession.setActionHandler('nexttrack', () => {
           forSaleInterlude.play();
         });
-      }
-      
-      audio.addEventListener("error", function(event) {
-        console.error("Failed to play audio: ", event.target.error.code);
-      });
-
-      let context = new (window.AudioContext || window.webkitAudioContext)();
-      let source = context.createMediaElementSource(audio);
-      source.connect(context.destination);
-      source.start();
+      };
+    
     });
 });
