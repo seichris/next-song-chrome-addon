b4ff60b0969206e278f10a2ceb89d9c37f3d8e6a
Chris <25006584+seichris@users.noreply.github.com>
2023-02-06 18:34:46 +0100

    Update background.js

--- src/bg/background.js
+++ src/bg/background.js
@@ -32,8 +32,19 @@ chrome.commands.onCommand.addListener(function(command) {
       navigator.mediaSession.metadata = new MediaMetadata(nextSong);
 
       // Play the next song
-      let audio = new HTMLAudioElement();
-      audio.src = "next_song_url";
+      let u = new HTMLAudioElement();
+      let forSaleInterlude = new HTMLAudioElement();
+
+      if ('mediaSession' in navigator) {
+        navigator.mediaSession.setActionHandler('previoustrack', () => {
+          u.play();
+        });
+        navigator.mediaSession.setActionHandler('nexttrack', () => {
+          forSaleInterlude.play();
+        });
+      }
+
+      
       audio.addEventListener("error", function(event) {
         console.error("Failed to play audio: ", event.target.error.code);
       });
