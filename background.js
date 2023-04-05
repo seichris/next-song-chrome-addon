57188bc2e0783bb01795bbb7b0a526973b061391
seichris <cerujurasa@gmail.com>
2023-04-05 17:45:53 +0400

    works for spotify from any current tab

--- background.js
+++ background.js
@@ -1,14 +1,37 @@
-chrome.commands.onCommand.addListener((command) => {
+let lastPlayedTabId;
+
+chrome.runtime.onMessage.addListener(async (message, sender) => {
+  console.log('Message received:', message, 'Sender:', sender);
+  if (message.action === 'media_played') {
+    lastPlayedTabId = sender.tab.id;
+    console.log(`Last played tab ID set to ${lastPlayedTabId}`);
+  }
+});
+
+chrome.commands.onCommand.addListener(async (command) => {
+  console.log(`Command received: ${command}`);
   if (command === 'next_song') {
-    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    if (lastPlayedTabId) {
+      console.log(`Executing content script on tab ${lastPlayedTabId}`);
       try {
-        await chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
+        chrome.scripting.executeScript({
+          target: { tabId: lastPlayedTabId },
           files: ['content_script.js'],
         });
       } catch (error) {
         console.error('Error executing script:', error);
       }
-    });
+    } else {
+      console.log('No last played tab ID found. Injecting content script into the active tab.');
+      try {
+        const [currentTab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.scripting.executeScript({
+          target: { tabId: currentTab.id },
+          files: ['content_script.js'],
+        });
+      } catch (error) {
+        console.error('Error executing script:', error);
+      }
+    }
   }
 });
--- content_script.js
+++ content_script.js
@@ -1,18 +1,35 @@
-(function () {
+console.log('Content script loaded.');
+
+document.addEventListener('click', () => {
+  console.log('Click event detected.');
+  chrome.runtime.sendMessage({ action: 'media_played' });
+});
+
+function playNextSong() {
+  console.log('playNextSong function called.');
+
   // Supported websites
   const nextButtons = {
     'open.spotify.com': '.player-controls__buttons button[data-testid="control-button-skip-forward"]',
     'music.youtube.com': 'ytmusic-player-bar .next-button',
-    'soundcloud.com': '.playControls__next'
+    'soundcloud.com': '.playControls__next',
+    'www.youtube.com': 'ytd-player .ytp-next-button'
   };
 
   for (const site in nextButtons) {
     if (window.location.hostname.includes(site)) {
+      console.log(`Site matched: ${site}`);
       const nextButton = document.querySelector(nextButtons[site]);
       if (nextButton) {
+        console.log('Next button found and clicked.');
         nextButton.click();
         break;
+      } else {
+        console.log('Next button not found.');
       }
     }
   }
-})();
+}
+
+// Call playNextSong() when the content script is executed
+playNextSong();
