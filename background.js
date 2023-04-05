17faf3e75a2ad9213522a7047ab9de106f2cb12a
seichris <cerujurasa@gmail.com>
2023-04-05 15:33:46 +0400

    make it work for current tab

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
 chrome.commands.onCommand.addListener((command) => {
   if (command === 'next_song') {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.executeScript(tabs[0].id, { file: 'content_script.js' });
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      try {
+        await chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          files: ['content_script.js'],
+        });
+      } catch (error) {
+        console.error('Error executing script:', error);
+      }
     });
   }
 });
