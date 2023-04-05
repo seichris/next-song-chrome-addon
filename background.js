b114a1bd4e452afa870a7b96480286a4832bad10
seichris <cerujurasa@gmail.com>
2023-04-05 17:48:35 +0400

    works for any media from any current tab

--- background.js
+++ background.js
@@ -11,27 +11,27 @@ chrome.runtime.onMessage.addListener(async (message, sender) => {
 chrome.commands.onCommand.addListener(async (command) => {
   console.log(`Command received: ${command}`);
   if (command === 'next_song') {
-    if (lastPlayedTabId) {
-      console.log(`Executing content script on tab ${lastPlayedTabId}`);
-      try {
-        chrome.scripting.executeScript({
-          target: { tabId: lastPlayedTabId },
-          files: ['content_script.js'],
-        });
-      } catch (error) {
-        console.error('Error executing script:', error);
+    try {
+      const tabs = await chrome.tabs.query({ url: ['*://*.youtube.com/*', '*://*.open.spotify.com/*', '*://*.music.youtube.com/*', '*://*.soundcloud.com/*'] });
+      const activeTabs = tabs.filter(tab => tab.active && tab.audible);
+      if (activeTabs.length) {
+        lastPlayedTabId = activeTabs[0].id;
       }
-    } else {
-      console.log('No last played tab ID found. Injecting content script into the active tab.');
-      try {
-        const [currentTab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        chrome.scripting.executeScript({
-          target: { tabId: currentTab.id },
-          files: ['content_script.js'],
-        });
-      } catch (error) {
-        console.error('Error executing script:', error);
+      if (lastPlayedTabId) {
+        console.log(`Executing content script on tab ${lastPlayedTabId}`);
+        try {
+          chrome.scripting.executeScript({
+            target: { tabId: lastPlayedTabId },
+            files: ['content_script.js'],
+          });
+        } catch (error) {
+          console.error('Error executing script:', error);
+        }
+      } else {
+        console.log('No active tab with media content found.');
       }
+    } catch (error) {
+      console.error('Error querying tabs:', error);
     }
   }
 });
